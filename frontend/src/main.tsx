9aee1839ff2494e4bce211337219e1d905da2fd8
Alexander Bello <abello-r@Alex--M3-Pro.local>
2024-07-16 14:41:31 +0200

    Test4

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -10,10 +10,10 @@ console.log(import.meta.env.KINDE_REDIRECT_URI)
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<KindeProvider
-			clientId={import.meta.env.KINDE_CLIENT_ID}
-			domain={import.meta.env.KINDE_DOMAIN}
-			redirectUri={import.meta.env.KINDE_REDIRECT_URI}
-			logoutUri={import.meta.env.KINDE_LOGOUT_URI}
+			clientId={import.meta.env.VITE_KINDE_CLIENT_ID}
+			domain={import.meta.env.VITE_KINDE_DOMAIN}
+			redirectUri={import.meta.env.VITE_KINDE_REDIRECT_URI}
+			logoutUri={import.meta.env.VITE_KINDE_LOGOUT_URI}
 		>
 			<App />
 		</KindeProvider>
