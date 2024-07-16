95dee3016c3ce055fcf21e73937f7fadf5a603ca
Alexander Bello <abello-r@Alex--M3-Pro.local>
2024-07-16 14:44:16 +0200

    Test4

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 import './index.css'
 
 console.log(import.meta.env)
-console.log(import.meta.env.KINDE_REDIRECT_URI)
+console.log(import.meta.env.VITE_KINDE_REDIRECT_URI)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
