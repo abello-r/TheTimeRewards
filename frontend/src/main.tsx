e89900fde71a56b57de74d3b1014642d3e975645
Alexander Bello <abello-r@Alex--M3-Pro.local>
2024-07-16 14:49:34 +0200

    Succesfull deploy on netlify

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,9 +4,6 @@ import { KindeProvider } from "@kinde-oss/kinde-auth-react";
 import App from './App.tsx'
 import './index.css'
 
-console.log(import.meta.env)
-console.log(import.meta.env.VITE_KINDE_REDIRECT_URI)
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<KindeProvider
